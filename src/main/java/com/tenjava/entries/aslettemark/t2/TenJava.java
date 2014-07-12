e76bf628bcc37736f5d5388a2c6982e4122b933a
Aksel Slettemark <akselslettemark@gmail.com>
2014-07-12 12:10:24 +0200

    First commit

--- src/main/java/com/tenjava/entries/aslettemark/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/aslettemark/t2/TenJava.java
@@ -1,5 +1,41 @@
 package com.tenjava.entries.aslettemark.t2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Logger;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+    
+    public Logger log = this.getLogger();
+    public HashMap<Material, Integer> materials = new HashMap<Material, Integer>();
+    public ArrayList<Player> droppers = new ArrayList<Player>();
+    
+    @Override
+    public void onEnable() {
+        log.info("Hi.");
+        loadMaterials();
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+    
+    @EventHandler
+    void onDrop(PlayerDropItemEvent event) {
+        
+    }
+    
+    private void loadMaterials() {
+        materials.put(Material.COAL, 8);
+        materials.put(Material.IRON_INGOT, 10);
+        materials.put(Material.IRON_ORE, 8);
+    }
+    
+}
